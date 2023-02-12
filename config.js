451b7195cc7ad073a6311194188364ace2586a59
sandaru07 <108501284+sandaru07@users.noreply.github.com>
2023-02-12 21:01:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
-INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
+FOOTER: process.env.FOOTER === undefined ? 's.id/Contact-Me' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === undefined ? '*✅*' : process.env.CAPTION,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
+INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed*' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=x7YnxS7A#v9i9czAotWcaJLysdVegcNtx_yT8VxkKkOiIyXn54UM' : process.env.SESSION_ID
 };
